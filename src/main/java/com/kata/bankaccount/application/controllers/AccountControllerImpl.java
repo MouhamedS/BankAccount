13c553c2929491cacd1964e1972d402523b9ad0b
Mouhamed Seydi <seydimouhamed200@gmail.com>
2024-01-20 23:43:27 +0100

    feat(controller): externalize openapi config into a interface

--- src/main/java/com/kata/bankaccount/application/controllers/AccountControllerImpl.java
+++ src/main/java/com/kata/bankaccount/application/controllers/AccountControllerImpl.java
@@ -4,17 +4,9 @@ package com.kata.bankaccount.application.controllers;
 import com.kata.bankaccount.application.mapper.TransactionResourceMapper;
 import com.kata.bankaccount.application.resources.TransactionResource;
 import com.kata.bankaccount.domain.ports.incoming.AccountService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Tag(name ="BankAccount API", description = "API to do some operations on bank account")
+@Tag(name = "BankAccount API", description = "API to do some operations on bank account")
 @Slf4j
 @RestController(value = "/api/v1/accounts")
 @RequiredArgsConstructor
@@ -42,42 +34,18 @@ public class AccountControllerImpl implements AccountController {
         this.accountService.deposit(amount, accountId, clientId);
     }
 
-    @Operation(summary = "Withdraw from bank account")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Withdraw done"),
-            @ApiResponse(responseCode = "400", description = "Invalid Transaction", content = {
-                    @Content
-            }),
-            @ApiResponse(responseCode = "402", description = "Account or Client not Found", content = {
-                    @Content
-            }),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
-
-    })
     @PostMapping(value = "/{accountId}/clients/{clientId}/withdraw/{amount}")
-    public void withdrawFromAccount(@Parameter(description = "Account identification number",required = true, example = "1145") @PathVariable Long accountId,
-                                    @Parameter(description = "Client identification number",required = true, example = "1145") @PathVariable Long clientId,
-                                    @Parameter(description = "Amount to withdraw from the account",required = true, example = "1500")@PathVariable BigDecimal amount) {
+    public void withdrawFromAccount(@PathVariable Long accountId,
+                                    @PathVariable Long clientId,
+                                    @PathVariable BigDecimal amount) {
         log.info("API /accounts/{accountId}/clients/{clientId}/{amount} with parameters: " +
                 "{} , {}, {}", accountId, clientId, amount);
         this.accountService.withdraw(amount, accountId, clientId);
     }
 
-    @Operation(summary = "Get list of the transactions")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Deposit done", content = {
-                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            array = @ArraySchema(schema = @Schema(implementation = TransactionResource.class)))
-            }),
-            @ApiResponse(responseCode = "402", description = "Account or Client not Found", content = {
-                    @Content
-            }),
-            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content)
-
-    })
     @GetMapping(value = "/{accountId}/clients/{clientId}")
-    public List<TransactionResource> getAccountHistory(@Parameter(description = "Account identification number",required = true, example = "1145") @PathVariable Long accountId,
-                                                       @Parameter(description = "Client identification number",required = true, example = "1145") @PathVariable Long clientId) {
+    public List<TransactionResource> getAccountHistory(@PathVariable Long accountId,
+                                                       @PathVariable Long clientId) {
 
         return this.transactionResourceMapper.toResources(this.accountService.transactions(accountId, clientId));
     }
